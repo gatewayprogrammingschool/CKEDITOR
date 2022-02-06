7349563c2bef1b2b25eee2e1dbea637968b6fedf
The Sharp Ninja <ninja@thesharp.ninja>
2022-02-06 06:18:53 -0600

    Initial Commit

---
+++ build-config.js
@@ -0,0 +1,107 @@
+﻿/**
+ * @license Copyright (c) 2003-2022, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
+ */
+
+/**
+ * This file was added automatically by CKEditor builder.
+ * You may re-use it at any time to build CKEditor again.
+ *
+ * If you would like to build CKEditor online again
+ * (for example to upgrade), visit one the following links:
+ *
+ * (1) https://ckeditor.com/cke4/builder
+ *     Visit online builder to build CKEditor from scratch.
+ *
+ * (2) https://ckeditor.com/cke4/builder/85e3fb589fa995a18a87acbd7e939328
+ *     Visit online builder to build CKEditor, starting with the same setup as before.
+ *
+ * (3) https://ckeditor.com/cke4/builder/download/85e3fb589fa995a18a87acbd7e939328
+ *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
+ *
+ * NOTE:
+ *    This file is not used by CKEditor, you may remove it.
+ *    Changing this file will not change your CKEditor configuration.
+ */
+
+var CKBUILDER_CONFIG = {
+	skin: 'moono-lisa',
+	preset: 'basic',
+	ignore: [
+		'.DS_Store',
+		'.bender',
+		'.editorconfig',
+		'.gitattributes',
+		'.gitignore',
+		'.idea',
+		'.jscsrc',
+		'.jshintignore',
+		'.jshintrc',
+		'.mailmap',
+		'.npm',
+		'.nvmrc',
+		'.travis.yml',
+		'bender-err.log',
+		'bender-out.log',
+		'bender.ci.js',
+		'bender.js',
+		'dev',
+		'gruntfile.js',
+		'less',
+		'node_modules',
+		'package-lock.json',
+		'package.json',
+		'tests'
+	],
+	plugins : {
+		'SPImage' : 1,
+		'SimpleImage' : 1,
+		'about' : 1,
+		'basicstyles' : 1,
+		'ccmssourcedialog' : 1,
+		'ckeditortablecellsselection' : 1,
+		'clipboard' : 1,
+		'contents' : 1,
+		'enterkey' : 1,
+		'entities' : 1,
+		'floatingspace' : 1,
+		'indentlist' : 1,
+		'link' : 1,
+		'list' : 1,
+		'markdown' : 1,
+		'maximize' : 1,
+		'nbsp' : 1,
+		'pagebreak' : 1,
+		'pastecode' : 1,
+		'pastefromgdocs' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'resizewithwindow' : 1,
+		'savemarkdown' : 1,
+		'selectall' : 1,
+		'selectallcontextmenu' : 1,
+		'showborders' : 1,
+		'simple-image-browser' : 1,
+		'simpleImageUpload' : 1,
+		'spreadsheet' : 1,
+		'stylesheetparser' : 1,
+		'stylesheetparser-fixed' : 1,
+		'syntaxhighlight' : 1,
+		'table' : 1,
+		'tableresize' : 1,
+		'tablesorter' : 1,
+		'tabletoolstoolbar' : 1,
+		'textindent' : 1,
+		'toolbar' : 1,
+		'uicolor' : 1,
+		'undo' : 1,
+		'uploadfile' : 1,
+		'uploadimage' : 1,
+		'widgetcontextmenu' : 1,
+		'widgettemplatemenu' : 1,
+		'youtube' : 1
+	},
+	languages : {
+		'en' : 1
+	}
+};
